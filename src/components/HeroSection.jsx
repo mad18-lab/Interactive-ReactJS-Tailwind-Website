dd16d8d3f17c517093d90785cb4c78d3d30c1a48
Madhav <guptamadhav69@gmail.com>
2024-05-27 16:11:26 +0530

    commit changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Typed } from 'react-typed';
 
 const HeroSection = () => {
   return (
-    <div className='text-white'>
-      <div className='max-w-[800px] mt-[186px] w-full h-[500px] mx-auto text-center flex flex-col'>
+    <div className='text-white w-full'>
+      <div className='max-w-[1280px] mt-[186px] h-[500px] mx-auto text-center flex flex-col'>
         <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
         <div className='md:text-4xl sm:text-3xl text-xl font-bold mb-4 p-2'>
-            <p>Fast, flexible financing for </p>
+            <p>Fast, flexible financing</p>
         </div>
         <p className='md:text-2xl sm:px-5 font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC & SaaS platforms.</p>
         <div>
